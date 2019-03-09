27cbc913bb47432e9f0b17078698b7854aba9416
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-03-09 15:07:57 +0900

    グリーンコード化。

--- src/SyncWait.java
+++ src/SyncWait.java
@@ -1,34 +1,30 @@
 //在庫管理プログラム
 
-class SyncInvertory {
+class SyncInvert {
     private int count; //在庫数
 
-    public SyncInvertory() {
+    SyncInvert() {
         count = 50;
     }
 
-    public synchronized void ship(int amount) {
+    synchronized void ship(int amount) throws InterruptedException {
         //在庫が足りるか
         if (count < amount) {
             System.out.println("================ ship() 待機.");
             //入庫待ち
-            try {
-                wait(); //入庫されるまで待機
-            } catch (InterruptedException e) {}
+            wait(); //入庫されるまで待機
             System.out.println("================ ship() 解放.");
         }
         //在庫数の減算
         count -= amount;
         notify(); //待ち状態のスレッドへ解除通知
-        try {
-            Thread.sleep((long) (Math.random() * 1000));
-        } catch (InterruptedException e) {}
+        Thread.sleep((long) (Math.random() * 1000));
         System.out.println("今回出庫した数 : " + amount);
         System.out.println("現在の在庫数 : " + count);
         System.out.println("----------------------");
     }
 
-    public synchronized void arrive(int amount) {
+    synchronized void arrive(int amount) throws InterruptedException {
         //在庫最大数
         if ((count + amount) > 50) {
             System.out.println("================ arrive() 待機.");
@@ -41,10 +37,8 @@ class SyncInvertory {
         //在庫数の加算
         count += amount;
         notify(); //待ち状態のスレッドへ解除通知
-        try {
-            Thread.sleep((long) (Math.random() * 1000));
-        } catch (InterruptedException e) {}
-            System.out.println("今回加算した数 : " + amount);
+        Thread.sleep((long) (Math.random() * 1000));
+        System.out.println("今回加算した数 : " + amount);
             System.out.println("現在の在庫数 : " + count);
             System.out.println("----------------------");
         }
@@ -52,9 +46,9 @@ class SyncInvertory {
 
     class Seller extends Thread {
         private int amount; //引き落とし数
-        private SyncInvertory si;
+        private SyncInvert si;
 
-        public Seller(SyncInvertory si, int amount) {
+        Seller(SyncInvert si, int amount) {
             this.si = si;
             this.amount = amount;
         }
@@ -62,16 +56,20 @@ class SyncInvertory {
         public void run() {
             //在庫数減産用メソッド呼び出し
             while (true) {
-                si.ship(amount);
+                try {
+                    si.ship(amount);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     class Buyer extends Thread {
         private int amount; //補充数
-        private SyncInvertory si;
+        private SyncInvert si;
 
-        public Buyer(SyncInvertory si, int amount) {
+        Buyer(SyncInvert si, int amount) {
             this.si = si;
             this.amount = amount;
         }
@@ -79,14 +77,18 @@ class SyncInvertory {
         public void run() {
             // 在庫補充用メソッド呼び出し
             while (true) {
-                si.arrive(amount);
+                try {
+                    si.arrive(amount);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     public class SyncWait {
         public static void main(String[] args) {
-            SyncInvertory si = new SyncInvertory();
+            SyncInvert si = new SyncInvert();
             Seller seller = new Seller(si, 15);
             Buyer buyer = new Buyer(si, 30);
 
@@ -96,19 +98,49 @@ class SyncInvertory {
     }
 
 /*
-            今回出庫した数 : 15
-            現在の在庫数 : 35
-            ----------------------
-            今回出庫した数 : 15
-            現在の在庫数 : 20
-            ----------------------
-            今回出庫した数 : 15
-            現在の在庫数 : 5
-            ----------------------
-            ================ ship() 待機.
-            今回加算した数 : 30
-            現在の在庫数 : 35
-            ----------------------
 
-            ~~~繰り返し~~~
+今回出庫した数 : 15
+現在の在庫数 : 35
+----------------------
+今回出庫した数 : 15
+現在の在庫数 : 20
+----------------------
+今回加算した数 : 30
+現在の在庫数 : 50
+----------------------
+================ arrive() 待機.
+今回出庫した数 : 15
+現在の在庫数 : 35
+----------------------
+今回出庫した数 : 15
+現在の在庫数 : 20
+----------------------
+今回出庫した数 : 15
+現在の在庫数 : 5
+----------------------
+================ ship() 待機.
+================ arrive() 解放.
+今回加算した数 : 30
+現在の在庫数 : 35
+----------------------
+================ arrive() 待機.
+================ ship() 解放.
+今回出庫した数 : 15
+現在の在庫数 : 20
+----------------------
+今回出庫した数 : 15
+現在の在庫数 : 5
+----------------------
+================ ship() 待機.
+================ arrive() 解放.
+今回加算した数 : 30
+現在の在庫数 : 35
+----------------------
+================ arrive() 待機.
+================ ship() 解放.
+今回出庫した数 : 15
+現在の在庫数 : 20
+----------------------
+
+~~~繰り返し~~~
 */
